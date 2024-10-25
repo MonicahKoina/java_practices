49932dcc2ae5273a27b532371ebdabbe337d3a24
Monicah Koina <monicahkoina1@gmail.com>
2024-10-25 09:36:55 +0300

    initial commit

---
+++ arraylist.java
@@ -0,0 +1,14 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class arraylist {
+    public static void main(String[] args) {
+        List<String> food = new ArrayList<>();
+        food.add("pizza");
+        food.add("Shawarma");
+        food.add("fries");
+        System.out.println(food);//output [pizza, Shawarma, fries]
+    }
+}
