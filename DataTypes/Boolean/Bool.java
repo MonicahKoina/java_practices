b18fc6521a54d49f389e985c5a6b86750675fb95
Monicah Koina <monicahkoina1@gmail.com>
2024-10-28 00:47:22 +0300

    Boolean

---
+++ DataTypes/Boolean/Bool.java
@@ -0,0 +1,14 @@
+package Tracom.DataTypes.Boolean;
+
+public class Bool {
+    //A boolean data type is a type used with logic/boolean values
+    //A boolean value represents true or false
+    public static void main(String[] args) {
+        boolean isAlive = true;
+        if (isAlive) {
+            System.out.println("Alive");
+        }else {
+            System.out.println("Not Alive"); //if I had initialized the boolean to be false my output would be "Not alive"
+        }
+    }
+}
