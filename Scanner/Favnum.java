7f99f3d5f8a3f291c6e139bdadb84240141c23cc
Monicah Koina <monicahkoina1@gmail.com>
2024-10-29 22:14:13 +0300

    get user's favourite number

---
+++ Scanner/Favnum.java
@@ -0,0 +1,13 @@
+package Tracom.Scanner;
+
+import java.util.Scanner;
+
+public class Favnum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your favourite number: ");
+        int favourite_number = input.nextInt();
+        System.out.println(favourite_number + " 13is also my favourite number");
+
+    }
+}
