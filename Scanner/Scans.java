ff04078d3d3bff8103904cbae1e35bda396d92c0
Monicah Koina <monicahkoina1@gmail.com>
2024-10-29 16:17:16 +0300

    Scanner class

---
+++ Scanner/Scans.java
@@ -0,0 +1,10 @@
+package Tracom.Scanner;
+
+import java.util.Scanner;
+
+public class Scans {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
