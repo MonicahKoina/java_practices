a74b36ce78bd55d7b08c0a0544ce8a2d7256e8f4
Monicah Koina <monicahkoina1@gmail.com>
2024-11-02 00:03:02 +0300

    get the 7th fibonacci number

---
+++ Loops/Fibonacci.java
@@ -0,0 +1,22 @@
+package Tracom.Loops;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        //Find the nth fibonacci number
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while (count <= n){
+            int temp = b;
+            b += a;
+            a = temp;
+            count++;
+
+        }
+        System.out.println(b);//input 7 .  output=13(13 is the 7th fibonacci number)
+    }
+}
